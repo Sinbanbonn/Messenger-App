3196b45923c18d13b5fcd44178520d5d0a928f59
Sinbanbonn <logvinov0607@icloud.com>
2023-05-07 13:04:19 +0300

    Project was created

---
+++ messenger/Controllers/ConversationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messenger
+//
+//  Created by Андрей Логвинов on 5/7/23.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
